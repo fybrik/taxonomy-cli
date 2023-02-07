c5107eaa973519fb2eaf4a3b9908217e23ffc85b
Vicky-Goldin <victoria.goldin@ibm.com>
2023-02-07 12:59:09 +0200

    Improve tests structure
    Signed-off-by: Vicky-Goldin <victoria.goldin@ibm.com>

--- test/integration_test.go
+++ test/integration_test.go
@@ -19,38 +19,39 @@ func TestIntegration(t *testing.T) {
 }
 
 var _ = Describe("Taxonomy-CLI integration tests", Ordered, func() {
-	var programPath string
+	programPath := os.Getenv("ABS_EXEC_PATH")
+	if programPath == "" {
+		Fail("The program path is not set, the tests should be run by 'make test'.")
+	}
+
+	testdataDir := "./testdata/"
 	var command *exec.Cmd
 	var errCmd error
-
-	BeforeAll(func() {
-		programPath = os.Getenv("ABS_EXEC_PATH")
-	})
 
 	Describe("Check validate command", func() {
 		var jsonFilePath string
 		var output []byte
 
-		Context("on a proper json file", func() {
+		Context("on a valid json file", func() {
 			BeforeEach(func() {
-				jsonFilePath = "./testdata/base.json"
+				jsonFilePath = testdataDir + "base.json"
 				command = exec.Command(programPath, "validate", jsonFilePath)
 				output, errCmd = command.Output()
 			})
 			It("should pass validation", func() {
-				Expect(errCmd).To(BeNil())
+				Expect(errCmd).NotTo(HaveOccurred())
 				Expect(output).To(ContainSubstring("validated successfully"))
 			})
 		})
 
 		Context("on a corrupted json file", func() {
 			BeforeEach(func() {
-				jsonFilePath = "./testdata/corrupted.json"
+				jsonFilePath = testdataDir + "corrupted.json"
 				command = exec.Command(programPath, "validate", jsonFilePath)
 				errCmd = command.Run()
 			})
 			It("should fail with error", func() {
-				Expect(errCmd).To(Not(BeNil()))
+				Expect(errCmd).To(HaveOccurred())
 			})
 		})
 	})
@@ -61,55 +62,55 @@ var _ = Describe("Taxonomy-CLI integration tests", Ordered, func() {
 		var dataExpected, dataOutput interface{}
 		var errReadExpected, errReadOutput error
 
-		Context("on a proper base", func() {
+		Context("on a valid base", func() {
 			BeforeEach(func() {
-				basePath = "./testdata/base.json"
-				outputPath = "./testdata/output_only_base.json"
+				basePath = testdataDir + "base.json"
+				outputPath = testdataDir + "output_only_base.json"
 				command = exec.Command(programPath, "compile", "-b", basePath, "-o", outputPath)
 				errCmd = command.Run()
 				dataExpected, errReadExpected = os.ReadFile(basePath)
 				dataOutput, errReadOutput = os.ReadFile(outputPath)
 			})
 			It("should create the same base file", func() {
-				Expect(errCmd).To(BeNil())
-				Expect(errReadExpected).To(BeNil())
-				Expect(errReadOutput).To(BeNil())
+				Expect(errCmd).NotTo(HaveOccurred())
+				Expect(errReadExpected).NotTo(HaveOccurred())
+				Expect(errReadOutput).NotTo(HaveOccurred())
 				Expect(reflect.DeepEqual(dataExpected, dataOutput)).To(BeTrue())
 			})
 		})
 
-		Context("on a proper base with a layer", func() {
+		Context("on a valid base with a layer", func() {
 			var layerPath, expectedPath string
 
 			BeforeEach(func() {
-				basePath = "./testdata/base.json"
-				layerPath = "./testdata/appinfo.yaml"
-				outputPath = "./testdata/output_base_with_layer.json"
-				expectedPath = "./testdata/expected_base_and_appinfo.json"
+				basePath = testdataDir + "base.json"
+				layerPath = testdataDir + "appinfo.yaml"
+				outputPath = testdataDir + "output_base_with_layer.json"
+				expectedPath = testdataDir + "expected_base_and_appinfo.json"
 				command = exec.Command(programPath, "compile", "-b", basePath, "-o", outputPath, layerPath)
 				errCmd = command.Run()
 				dataExpected, errReadExpected = os.ReadFile(expectedPath)
 				dataOutput, errReadOutput = os.ReadFile(outputPath)
 			})
 			It("should produce the same file as expected", func() {
-				Expect(errCmd).To(BeNil())
-				Expect(errReadExpected).To(BeNil())
-				Expect(errReadOutput).To(BeNil())
+				Expect(errCmd).NotTo(HaveOccurred())
+				Expect(errReadExpected).NotTo(HaveOccurred())
+				Expect(errReadOutput).NotTo(HaveOccurred())
 				Expect(reflect.DeepEqual(dataExpected, dataOutput)).To(BeTrue())
 			})
 		})
 
 		Context("on a corrupted base", func() {
 			BeforeEach(func() {
-				basePath = "./testdata/corrupted.json"
-				outputPath = "./testdata/output_corrupted_base.json"
+				basePath = testdataDir + "corrupted.json"
+				outputPath = testdataDir + "output_corrupted_base.json"
 				command = exec.Command(programPath, "compile", "-b", basePath, "-o", outputPath)
 				errCmd = command.Run()
 				_, errReadOutput = os.ReadFile(outputPath)
 			})
 			It("should create the same base file", func() {
-				Expect(errCmd).To(Not(BeNil()))
-				Expect(errReadOutput).To(Not(BeNil()))
+				Expect(errCmd).To(HaveOccurred())
+				Expect(errReadOutput).To(HaveOccurred())
 			})
 		})
 	})
